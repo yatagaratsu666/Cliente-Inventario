c22a8a90fa8afb45d9215059442819def89f0519
Drift <diego.suarez1637@gmail.com>
2025-08-26 19:50:01 -0500

    Si camilo, ya andamos haciendo tu cliente, deja de llorar

--- src/app/rooms-list/rooms-list.component.ts
+++ src/app/rooms-list/rooms-list.component.ts
@@ -11,6 +11,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css'],
   imports: [CommonModule, ReactiveFormsModule]
 })
 export class RoomsListComponent implements OnInit {

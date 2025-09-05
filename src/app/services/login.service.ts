66e2e05fe023601bad60281030394fadc059783a
Mikudayo <brenda.salcedoh@gmail.com>
2025-09-04 20:22:33 -0500

    Se modificó las vistas de batalla

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -5,15 +5,16 @@ import { Observable, of, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
-  private readonly USERNAME = 'admin';
-  private readonly PASSWORD = '1234';
+  private readonly USERNAME = 'admin'; // me falta poner el rol
+  // private readonly ROLE = 'admin'; // crear una interfaz para el usuario mas tarde :333
+  private readonly PASSWORD = '1234'; 
   private readonly USERNAME2 = 'user2';
   private readonly PASSWORD2 = '5678';
 
   constructor() {}
 
   login(username: string, password: string): Observable<boolean> {
-    if (username === this.USERNAME && password === this.PASSWORD) {
+    if (username === this.USERNAME && password === this.PASSWORD ) {
       localStorage.setItem('loggedIn', 'true');
       localStorage.setItem('username', username);
       return of(true);

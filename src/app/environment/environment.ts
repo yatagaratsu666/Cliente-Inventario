d8039888d9035c55b393b2127e5222cccbfe313a
Camanrofo <camanrofo@gmail.com>
2025-09-15 12:06:36 -0500

    Debug login y cambio en el formulario

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -12,5 +12,5 @@ export const environment = {
 
   inventarySocket: 'http://localhost:1882',
 
-  chatUrlSocket: 'http://speedorz.bucaramanga.upb.edu.co:4000'
+  chatUrlSocket: 'http://localhost:4000'
 };

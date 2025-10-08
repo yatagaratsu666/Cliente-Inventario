2f1f81a786f937b174341b5e7a7aa6420a39bd91
Drift <diego.suarez1637@gmail.com>
2025-10-07 23:31:18 -0500

    cambios para el despliegue

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -3,19 +3,19 @@ export const environment = {
   production: false,
 
   /** URL base de la API utilizada en testeo. */
-  apiUrl: 'http://146.148.77.95:1882',
+  apiUrl: 'http://136.112.163.5:1882', /* 'http://localhost:1882',*/
 
   /** URL base del servicio de batallas. */
   battleUrl: 'http://localhost:3000', //'http://34.57.149.206:3000',
 
   battleSocket: 'http://localhost:3000',
 
-  inventarySocket: 'http://146.148.77.95:1882',
+  inventarySocket: 'http://136.112.163.5:1882/',  /*'http://localhost:1882',*/
 
-  chatUrlSocket: 'http://localhost:4000',
+  chatUrlSocket: 'http://34.44.126.114:4000',
 
-  api: { base: 'http://localhost:3000/api' },
-  socket: { base: 'http://localhost:3000' },
+  api: { base: 'https://auction-deploy-272361825762.us-east1.run.app/api' },
+  socket: { base: 'https://auction-deploy-272361825762.us-east1.run.app' },
 
   usersUrl: 'https://thenexusbattles-771648021041.southamerica-east1.run.app'
 
